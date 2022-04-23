630ad74f864352d40bd20e5fbdcf8a9f29da45f9
hamzazaouya <hamzazaouya18@gmail.com>
2022-04-23 18:47:21 +0000

    start

---
+++ lib/auth/login.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Container(
+        height: 500,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+                child: SizedBox(
+                    height: 150,
+                    width: 150,
+                    child: Image.asset("images/logo.png"))),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Form(
+                  child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.person),
+                        hintText: "User Name",
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(width: 1))),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.key),
+                        hintText: "Password",
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(width: 1))),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(top: 10, bottom: 10),
+                    child: Row(children: [
+                      const Text("If you don't have account? "),
+                      InkWell(
+                        onTap: () {},
+                        child: const Text("Click here",style: TextStyle(color: Colors.blue),),
+                      )
+                    ]),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(minimumSize: Size(120, 40)),
+                    onPressed: () {},
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  )
+                ],
+              )),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
